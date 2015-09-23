520ebf64d516686b8e20e0af16cc062a1324d1de
Jespicas <espi@espipinxo.com>
2015-09-24 00:57:26 +0200

    Add new settings AutoSlideshow and the autoslideshow functionality

--- default.py
+++ default.py
@@ -5,6 +5,7 @@ import urllib
 import urlparse
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 from BeautifulSoup import BeautifulSoup 
 
 import requests
@@ -13,8 +14,11 @@ import requests
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
-
+print sys.argv
 xbmcplugin.setContent(addon_handle, 'movies')
+print addon_handle
+__settings__   = xbmcaddon.Addon(id="plugin.image.mongolfridayphotos")
+
 
 def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
@@ -27,7 +31,7 @@ r = requests.get('http://www.machacas.com/category/mongol-friday-photos/')
 data = r.text
 soup = BeautifulSoup(data)
 DivPagination = soup.body.findAll("div",{"class": "pagination"})
-#print DivPagination
+
 last = DivPagination[0].findAll('a')
 
 MaxPage = last[len(last)-1].attrs[0][1].split('/')[len(last[len(last)-1].attrs[0][1].split('/')) -1]
@@ -46,27 +50,41 @@ def AddItems(Url,currentPage,EndDirectory):
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,listitem=li, isFolder=True)
 
     if (EndDirectory):
-        print MaxPage
         if (currentPage +1) <= MaxPage:
             currentPage = currentPage +1
-            print currentPage
             NextPage = 'http://www.machacas.com/category/mongol-friday-photos/page/' + str(currentPage)
-            print NextPage
             url1 = build_url({'mode': 'next', 'foldername': 'Next','value':NextPage})
             li1 = xbmcgui.ListItem('Next' , iconImage='DefaultFolder.png')
             xbmcplugin.addDirectoryItem(handle=addon_handle, url=url1,listitem=li1, isFolder=True)
 
         xbmcplugin.endOfDirectory(addon_handle)
 
+def AddImagesInFolder(valors):
+    r = requests.get(valors)
+    data = r.text
+    soup = BeautifulSoup(data)
+    for link in soup.findAll("img",{"class":"lazy lazy-hidden"}):
+        print link.attrs[3][1]
+        url = link.attrs[3][1]
+        commands = []
+        script = ""
+        name = "Hello World"
+        arg = "ARGS"
+        runner = "XBMC.RunScript(" + str(script)+ ", " + str(arg) + ")"
+        commands.append(( str(name), runner, ))
+        li = xbmcgui.ListItem(url.split('/')[5] + ' Img', iconImage='DefaultVideo.png')
+        li.addContextMenuItems( commands )
+
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+    print mode
+    xbmcplugin.endOfDirectory(addon_handle)
+
 if mode is None:
     AddItems('http://www.machacas.com/category/mongol-friday-photos/',1,False)
     for Page in range(2,NumberPagination):
-        print Page
         if Page+1 == NumberPagination:
-            print "LastIteration"
             AddItems('http://www.machacas.com/category/mongol-friday-photos/page/'+str(Page),Page,True)
         else:
-            print "Iteration" + str(Page)
             AddItems('http://www.machacas.com/category/mongol-friday-photos/page/'+str(Page),Page,False)
 
 
@@ -86,14 +104,17 @@ elif mode[0] == 'folder':
     print foldername
     valors = args['value'][0]
     print valors
+    if __settings__.getSetting("SlideShow") == "true":
+        print "SlideShow auto true"
+        xbmc.executebuiltin( "SlideShow(plugin://plugin.image.mongolfridayphotos/?foldername="+foldername+"&mode=slideshow&value="+valors+",,notrandom)" )
 
-    r = requests.get(valors)
-    data = r.text
-    soup = BeautifulSoup(data)
-    for link in soup.findAll("img",{"class":"lazy lazy-hidden"}):
-        print link.attrs[3][1]
-        url = link.attrs[3][1]
-        li = xbmcgui.ListItem(url.split('/')[4] + ' Img', iconImage='DefaultVideo.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    
-    xbmcplugin.endOfDirectory(addon_handle)
+    AddImagesInFolder(valors)
+
+elif mode[0] == 'slideshow':
+    print "SlideShow"
+    foldername = args['foldername'][0]
+    print foldername
+    valors = args['value'][0]
+    print valors
+
+    AddImagesInFolder(valors)
